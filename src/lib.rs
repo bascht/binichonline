1835ea66608c45344c299fedaa5c087197cdc069
Sebastian Schulze <github.com@bascht.com>
2015-09-14 00:03:40 +0200

    Pull out stuff to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
-#[test]
-fn it_works() {
+pub mod ui {
+    extern crate term;
+
+    pub fn online() {
+        let mut t = term::stdout().unwrap();
+        t.fg(term::color::GREEN).unwrap();
+        writeln!(t, "Online").unwrap();
+
+    }
+    pub fn offline() {
+        let mut t = term::stdout().unwrap();
+        t.fg(term::color::RED).unwrap();
+        writeln!(t, "Offline").unwrap();
+
+    }
+    pub fn clear_screen() {
+        println!("\x1b[2J\x1b[1;1H");
+    }
+}
+
+pub mod net {
+    extern crate curl;
+
+    use self::curl::http;
+
+    pub fn is_online() -> bool {
+        let resp = http::handle()
+            .get("http://localhost:8000")
+            .exec();
+
+        match resp {
+            Ok(r) => true,
+            Err(e) => false
+        }
+    }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,24 @@
-extern crate curl;
-extern crate term;
+extern crate binichonline;
 
 use std::thread;
-use curl::http;
+use binichonline::ui;
+use binichonline::net;
 
 struct Looper {
     interval: u32
 }
 
 impl Looper {
-
-    fn is_online() -> bool {
-        let resp = http::handle()
-            .get("http://localhost:8000")
-            .exec();
-
-        match resp {
-            Ok(r) => true,
-            Err(e) => false
-        }
-    }
-
     pub fn run(&self) {
         loop {
-            let mut t = term::stdout().unwrap();
-            match Looper::is_online() {
-                true => {
-                    t.fg(term::color::GREEN).unwrap();
-                    writeln!(t, "Online").unwrap();
-
-                },
-                false =>
-                {
-                    t.fg(term::color::RED).unwrap();
-                    writeln!(t, "Offline").unwrap();
-                }
+            match net::is_online() {
+                true  => ui::online(),
+                false => ui::offline()
             }
             thread::sleep_ms(self.interval);
-            clear_screen();
+            ui::clear_screen();
         }
     }
-}
-
-fn clear_screen() {
-    println!("\x1b[2J\x1b[1;1H");
 }
 
 fn main() {
