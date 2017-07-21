d6582ca6a1bafcda75de31cb96c6b90f7f9ccd56
Sebastian Schulze <github.com@bascht.com>
2017-07-21 18:16:06 +0200

    Adapt to changes in std::time::Duration

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 extern crate binichonline;
 
 use std::thread;
+use std::time::Duration;
 use binichonline::ui;
 use binichonline::net;
 
 struct Looper {
-    interval: u32
+    interval: std::time::Duration
 }
 
 impl Looper {
     pub fn run(&self) {
         loop {
             ui::update(net::is_online());
-            thread::sleep_ms(self.interval);
+            thread::sleep(self.interval);
             ui::clear_screen();
         }
     }
 }
 
 fn main() {
-    Looper { interval: 1500 }.run();
+    Looper { interval: Duration::from_millis(1500) }.run();
 }
